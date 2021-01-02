56c71514f17c11ead4b4897cbb67727dbdda563f
Adriel dos Santos <adrieldossantos7@gmail.com>
2021-01-02 15:27:12 -0300

    API: criado todas as funcionalidades CRUD de usuários.

--- API/API/Controllers/UsersController.cs
+++ API/API/Controllers/UsersController.cs
@@ -36,5 +36,72 @@ namespace API.Controllers
             return BadRequest();
 
         }
+        [HttpGet]
+        public async Task<IActionResult> getUsers()
+        {
+            try
+            {
+                var result = await _repo.GetAllUsers();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest($"Erro: {e.Message}");
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getUserById(int id)
+        {
+            try
+            {
+                var result = await _repo.GetUserById(id);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro: {e.Message}");
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> updateUserById(int id, User model)
+        {
+            try
+            {
+                var user = await _repo.GetUserById(id);
+                if (user == null) return BadRequest("User não encontrado");
+
+                _repo.Update(model);
+
+                if(await _repo.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro: {e.Message}");
+            }
+            return BadRequest("Erro inesperado");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> deleteUser(int id)
+        {
+            try
+            {
+                var user = await _repo.GetUserById(id);
+                if (user == null) return BadRequest("Usuário não encontrado");
+                _repo.Delete(user);
+                if( await _repo.SaveChangesAsync())
+                {
+                    return Ok("Deletado");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"{e.Message}");
+            }
+            return BadRequest("Erro inesperado");
+        }
     }
 }
